48e8732139aef389d75378f2e0489127889781cf
escotoj <j.escoto14@gmail.com>
2023-07-03 15:18:24 -0700

    input form and buttons are rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [submittedValue, setSubmittedValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedValue(inputValue);
+    setInputValue('');
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
-"Workout Log App"
+      <h1>Solid Workout App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Log</button>
+      </form>
+      {submittedValue && (
+        <div>
+          <h2>Workout:</h2>
+          <p>{submittedValue}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;

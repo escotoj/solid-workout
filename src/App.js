2d9221661c4e8a55ba3151cf591f995348a286a2
escotoj <j.escoto14@gmail.com>
2023-07-03 21:18:08 -0700

    npm i a few materials and styled the form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, TextField, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+    padding: theme.spacing(3),
+  },
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: theme.spacing(2),
+  },
+  input: {
+    marginRight: theme.spacing(2),
+  },
+  submitButton: {
+    marginLeft: theme.spacing(2),
+  },
+  submittedValue: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   const [inputValue, setInputValue] = useState('');
   const [submittedValue, setSubmittedValue] = useState('');
 
@@ -15,16 +40,33 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Solid Workout App</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button type="submit">Log</button>
+    <div className={classes.root}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Solid Workout App
+      </Typography>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          className={classes.input}
+          variant="outlined"
+          label="Workout"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Button
+          className={classes.submitButton}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Log
+        </Button>
       </form>
       {submittedValue && (
-        <div>
-          <h2>Workout:</h2>
-          <p>{submittedValue}</p>
+        <div className={classes.submittedValue}>
+          <Typography variant="h6" component="h2" gutterBottom>
+            Workout:
+          </Typography>
+          <Typography variant="body1">{submittedValue}</Typography>
         </div>
       )}
     </div>
